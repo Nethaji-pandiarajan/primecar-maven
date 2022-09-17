ac2739712ef1e045f5c7a28396df48c6d9b6f3c5
Nethaji-pandiarajan <82449478+Nethaji-pandiarajan@users.noreply.github.com>
2022-09-17 18:45:25 +0530

    DB credentials changed

--- src/main/java/com/circuits99/utils/ConnectionUtils.java
+++ src/main/java/com/circuits99/utils/ConnectionUtils.java
@@ -11,9 +11,9 @@ public class ConnectionUtils {
 	public static Connection createConnection() {
 //		Properties prop = PropertyFileReader.getValues();
 		String DRIVER_CLASS = "org.postgresql.Driver";//prop.getProperty("DRIVER_CLASS");
-		String DB_URL = "jdbc:postgresql://ec2-3-222-74-92.compute-1.amazonaws.com/da5bl47d3v2b0g";//prop.getProperty("DB_URL");
-		String DB_USERNAME = "rnpkwzrskpimvc";//prop.getProperty("DB_USERNAME");
-		String DB_PASSWORD = "c4d7e243169c35550a284510ff2b4d67b5de4babeae9cf9a1c10cfe0967e1d0d";//prop.getProperty("DB_PASSWORD");
+		String DB_URL = "jdbc:postgresql://ec2-3-219-52-220.compute-1.amazonaws.com";//prop.getProperty("DB_URL");
+		String DB_USERNAME = "gcubzjzfqmutgq";//prop.getProperty("DB_USERNAME");
+		String DB_PASSWORD = "396e6a098f8ccc1b1fb929b9e2267f80e5f9b94b45356971be58b8b4208e9119";//prop.getProperty("DB_PASSWORD");
 		Connection connection = null;
 		try {
 			Class.forName(DRIVER_CLASS);
